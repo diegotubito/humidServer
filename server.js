67c9a5d6cdd826eb45a8e522730f2d5bba141d6f
Diego Gomez <dgomez@genium.io>
2023-02-19 21:43:46 -0300

    agregamos manejo de errores y creacion de nuevos usuarios

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 const express = require('express')
 const app = express()
 
-app.get('/user/list', function (req, res) {
+// Setup a Json format for body request
+app.use(express.json())
 
-    const mockUsers = {
-        name: 'Diego',
-        age: 45
+// lista de usuarios falsos
+const userDiego = {
+    name: 'Diego',
+    age: 45
+}
+const userLiz = {
+    name: 'Liz',
+    age: 27
+}
+
+const list = [userDiego, userLiz]
+
+
+
+app.get('/user', function (req, res) {
+  res.json(list)
+})
+
+app.post('/user', function (req, res) {
+    const body = req.body
+   
+    const name = body.name
+    if (name == null ) {
+        res.status(400).json({
+            message: 'Error el parametro name es obligatorio'
+        })
+        return
+    } else if (name == "") {
+        res.status(400).json({
+            message: 'Error el parametro name no puede estar vacio'
+        })
+
+    } else {
+        list.push(body)
+
+        res.status(201).json({
+            message: 'success'
+        })    
     }
-
-  res.json(mockUsers)
 })
 
 app.listen(3000, () => {
